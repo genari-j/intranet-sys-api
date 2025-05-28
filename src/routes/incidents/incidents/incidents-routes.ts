9884b2a0f218a302d195c08cac935280748e683e
João Genari <victor.olr@hotmail.com>
2025-05-27 21:06:46 -0300

    feat: cria nova rota para lidar com listagem dos incidents

--- src/routes/incidents/incidents/incidents-routes.ts
+++ src/routes/incidents/incidents/incidents-routes.ts
@@ -1,15 +1,18 @@
 import type { FastifyInstance, FastifyPluginAsync } from 'fastify'
 
 import { IncidentsController } from '~/controllers/index'
+import { IncidentsRepository } from '~/models/repositories/index'
 
 import { authMiddleware } from '~/middlewares/index'
 import { createStorage, incidentsPath } from '~/helpers/index'
 
 const upload = createStorage(incidentsPath)
 
-const controller = new IncidentsController()
+const controller = new IncidentsController(IncidentsRepository)
 
 const incidentsRoutes: FastifyPluginAsync = async (fastify: FastifyInstance) => {
+	fastify.get('/incidents', { preHandler: [authMiddleware] }, controller.getAll.bind(controller))
+
 	fastify.post(
 		'/incidents',
 		{ preHandler: [authMiddleware, upload.array('avatar')] },
