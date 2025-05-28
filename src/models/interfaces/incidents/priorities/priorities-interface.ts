6b9e669f166b8519ee0e2a279e67d3c1b4450d31
João Genari <victor.olr@hotmail.com>
2025-05-27 21:01:05 -0300

    feat: remove arquivos não necessários

--- src/models/interfaces/incidents/priorities/priorities-interface.ts
+++
@@ -1,5 +0,0 @@
-import type { GetPrioritiesResponse } from '~/@types/index'
-
-export interface PrioritiesInterfaceRepository {
-	findOneBy(field: string | number, value: string | number | undefined): Promise<GetPrioritiesResponse | null>
-}
--- src/models/interfaces/incidents/status/status-interface.ts
+++
@@ -1,5 +0,0 @@
-import type { GetStatusResponse } from '~/@types/index'
-
-export interface StatusInterfaceRepository {
-	findOneBy(field: string | number, value: string | number | undefined): Promise<GetStatusResponse | null>
-}
