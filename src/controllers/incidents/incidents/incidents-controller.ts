3325c563b493181603af6f5642d2c902b8bd173e
João Genari <victor.olr@hotmail.com>
2025-06-01 15:30:14 -0300

    feat: nova requisição p detalhar um chamado pelo ID

--- src/controllers/incidents/incidents/incidents-controller.ts
+++ src/controllers/incidents/incidents/incidents-controller.ts
@@ -3,9 +3,14 @@ import type { FastifyReply, FastifyRequest } from 'fastify'
 import type { IncidentsInterfaceRepository } from '~/models/interfaces/index'
 import { IncidentsService } from '~/services/usecases/index'
 
-import { decodeRequestAuthToken, unauthorized, created, internalError, ok } from '~/helpers/index'
+import { decodeRequestAuthToken, unauthorized, created, internalError, ok, notFound } from '~/helpers/index'
 import type { GetIncidentsResponse } from '~/@types/index'
-import { env, getIncidentsFiltersQuerySchema, createIncidentsBodySchema } from '~/validators/index'
+import {
+	env,
+	getIncidentsFiltersQuerySchema,
+	createIncidentsBodySchema,
+	incidentsParamsSchema,
+} from '~/validators/index'
 
 export class IncidentsController {
 	private readonly incidentsRepository: IncidentsInterfaceRepository
@@ -19,6 +24,8 @@ export class IncidentsController {
 			const { authorization } = request.headers
 			const user = await decodeRequestAuthToken(authorization)
 			if (!user) return unauthorized(reply)
+
+			const isAdmin = user?.profile?.name === 'Admin'
 
 			const query = request.query as {
 				page?: string
@@ -41,33 +48,18 @@ export class IncidentsController {
 				Number(limit),
 				filters,
 				user.department_id,
+				isAdmin,
 			)
 
 			const mappedIncidents = data.map((incident) => {
 				return {
 					id: incident.id,
 					code: incident.code,
-					active: incident.active,
-					deadline: incident.deadline,
 					title: incident.title,
-					description: incident.description,
-					register: { id: incident.register.id, name: incident.register.name },
-					category: { id: incident.category.id, name: incident.category.name },
 					priority: { id: incident.priority.id, name: incident.priority.name },
-					department: { id: incident.department.id, name: incident.department.name },
 					assigned: { id: incident?.user?.id, name: incident?.user?.name },
 					status: { id: incident.status.id, name: incident.status.name },
-					avatars: incident.incidentAvatars.map((incAvatar) => ({
-						id: incAvatar.id,
-						avatar: `http://localhost:3002/uploads/incidents/${incAvatar.avatar}`,
-					})),
-					logs: incident.incidentLogs.map((incLog) => ({
-						id: incLog.id,
-						title: incLog.title,
-						description: incLog.description,
-					})),
 					created_at: incident.created_at,
-					updated_at: incident.updated_at,
 				}
 			})
 
@@ -80,6 +72,45 @@ export class IncidentsController {
 					currentPage,
 				},
 			})
+		} catch (err) {
+			return internalError(reply, `Algo saiu como não esperado: ${err}`)
+		}
+	}
+
+	async getById(request: FastifyRequest, reply: FastifyReply) {
+		try {
+			const { id } = incidentsParamsSchema.parse(request.params)
+
+			const incidentById = await this.incidentsRepository.findIncidentById(id)
+			if (!incidentById) return notFound(reply, 'O chamado especificado não existe.')
+
+			const news = {
+				id: incidentById.id,
+				code: incidentById.code,
+				active: incidentById.active,
+				deadline: incidentById.deadline,
+				title: incidentById.title,
+				description: incidentById.description,
+				register: { id: incidentById.register.id, name: incidentById.register.name },
+				category: { id: incidentById.category.id, name: incidentById.category.name },
+				priority: { id: incidentById.priority.id, name: incidentById.priority.name },
+				department: { id: incidentById.department.id, name: incidentById.department.name },
+				assigned: { id: incidentById?.user?.id, name: incidentById?.user?.name },
+				status: { id: incidentById.status.id, name: incidentById.status.name },
+				avatars: incidentById.incidentAvatars.map((incAvatar) => ({
+					id: incAvatar.id,
+					avatar: `http://localhost:3002/uploads/incidents/${incAvatar.avatar}`,
+				})),
+				logs: incidentById.incidentLogs.map((incLog) => ({
+					id: incLog.id,
+					title: incLog.title,
+					description: incLog.description,
+				})),
+				created_at: incidentById.created_at,
+				updated_at: incidentById.updated_at,
+			}
+
+			return ok(reply, news)
 		} catch (err) {
 			return internalError(reply, `Algo saiu como não esperado: ${err}`)
 		}
