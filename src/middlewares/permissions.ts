8e0387fd7c08a1001e22d1e526cff1aaf4794852
João Genari <victor.olr@hotmail.com>
2025-06-24 18:01:57 -0300

    feat: novo middleware para checar permissões em rotas

--- src/middlewares/permissions.ts
+++ src/middlewares/permissions.ts
@@ -1,42 +1,16 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
-import { decodeRequestAuthToken, forbidden, unauthorized } from '~/helpers/index'
+import { decodeRequestAuthToken, unauthorized } from '~/helpers/index'
 
-export const adminRoutes = ['FAQ', 'Dashboard', 'Register News', 'Edit News']
-export const managerRoutes = ['FAQ', 'Register News']
-export const funRoutes = ['Systems']
-
-export const roles = {
-	admin: ['FAQ', 'Dashboard', 'Register News', 'Edit News'],
-	manager: ['FAQ', 'Register News'],
-	user: ['Systems'],
-}
-
-interface PermissionOptions {
-	requiredPermissions?: string[]
-	requiredRole?: keyof typeof roles
-}
-
-export const permMiddleware = ({ requiredPermissions = [], requiredRole }: PermissionOptions) => {
+export const permsMiddleware = (requiredPerms: string[]) => {
 	return async (request: FastifyRequest, reply: FastifyReply) => {
 		const { authorization } = request.headers
-		const user = await decodeRequestAuthToken(authorization)
-		if (!user) return unauthorized(reply)
+		const decodedUser = await decodeRequestAuthToken(authorization)
+		if (!decodedUser) return unauthorized(reply)
 
-		const userPermissions = user.profile.profilePermissions.map((perm) => perm.permission)
-
-		const hasPermissions = (required: string[]) => required.every((perm) => userPermissions.includes(perm))
-
-		if (requiredRole) {
-			const rolePermissions = roles[requiredRole]
-			if (!hasPermissions(rolePermissions)) {
-				return forbidden(reply, 'Você não possui permissão para acessar essa funcionalidade.')
-			}
-		}
-
-		if (requiredPermissions.length > 0 && !hasPermissions(requiredPermissions)) {
-			return forbidden(reply, 'Você não possui as permissões necessárias para acessar essa rota.')
-		}
-
-		return
+		const userPermissions = decodedUser.permissions.map((perm) => perm.permission.name)
+		const checkPermissions = requiredPerms.every((perm) => userPermissions.includes(perm))
+		if (!checkPermissions) return unauthorized(reply)
 	}
 }
+
+export const allowedPersonalIncidents = ['Visualizar chamados específicos']
